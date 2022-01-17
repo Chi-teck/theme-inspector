d151ddb20d4154bb019d509153f95d0ec398b81c
Chi <Chi@556138.no-reply.drupal.org>
2022-01-17 15:53:03 +0500

    Add 'More Link' preview plugin

---
+++ modules/theme_inspector_common/src/Plugin/ThemePreview/MoreLink.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\theme_inspector_common\Plugin\ThemePreview;
+
+use Drupal\Core\Url;
+use Drupal\theme_inspector\ThemePreviewPluginBase;
+
+/**
+ * A preview plugin for "More link".
+ *
+ * @ThemePreview(
+ *   id = "more_link",
+ *   label = @Translation("More Link"),
+ *   category = @Translation("Navigation"),
+ * )
+ */
+final class MoreLink extends ThemePreviewPluginBase {
+
+  public function build(string $variation): array {
+    return [
+      '#title' => 'Read more',
+      '#type' => 'more_link',
+      '#url' => Url::fromRoute('<current>'),
+    ];
+  }
+
+}
