2287cea06ea1818905c67a6f6264488df2444d46
Chi <Chi@556138.no-reply.drupal.org>
2022-01-17 12:04:13 +0500

    Add search input preview plugin

---
+++ modules/theme_inspector_form/src/Plugin/ThemePreview/Search.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\theme_inspector_form\Plugin\ThemePreview;
+
+/**
+ * Search input preview.
+ *
+ * @ThemePreview(
+ *   id = "search",
+ *   label = @Translation("Search"),
+ *   category = @Translation("Form Elements"),
+ *   variations = {
+ *     "default" = @Translation("Default"),
+ *     "disabled" = @Translation("Disabled"),
+ *   },
+ * )
+ */
+final class Search extends FormElementBase {
+
+  protected function getElement(string $variation): array {
+    return [
+      '#title' => $this->random()->title(),
+      '#type' => 'search',
+      '#description' => $this->random()->sentences(1),
+      '#disabled' => $variation === 'disabled',
+    ];
+  }
+
+}
