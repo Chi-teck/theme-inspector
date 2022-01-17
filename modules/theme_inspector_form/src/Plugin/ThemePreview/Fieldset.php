6024b029ac326435ce377155ab9cccf0cf9008c1
Chi <Chi@556138.no-reply.drupal.org>
2022-01-17 11:42:12 +0500

    Add fieldset preview plugin

---
+++ modules/theme_inspector_form/src/Plugin/ThemePreview/Fieldset.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\theme_inspector_form\Plugin\ThemePreview;
+
+/**
+ * Fieldset input preview.
+ *
+ * @ThemePreview(
+ *   id = "fieldset",
+ *   label = @Translation("Fieldset"),
+ *   category = @Translation("Form Elements"),
+ *   variations = {
+ *     "default" = @Translation("Default"),
+ *     "disabled" = @Translation("Disabled"),
+ *   },
+ * )
+ */
+final class Fieldset extends FormElementBase {
+
+  protected function getElement(string $variation): array {
+    return [
+      '#type' => 'fieldset',
+      '#title' => 'Author',
+      '#disabled' => $variation === 'disabled',
+      'first_name' => [
+        '#type' => 'textfield',
+        '#title' => 'First name',
+      ],
+      'last_name' => [
+        '#type' => 'textfield',
+        '#title' => 'Last name',
+      ],
+    ];
+  }
+
+}
