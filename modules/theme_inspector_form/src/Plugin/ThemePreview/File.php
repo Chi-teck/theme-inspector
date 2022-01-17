916c5c5d1b42176a41ec2510dfce97901182c098
Chi <Chi@556138.no-reply.drupal.org>
2022-01-17 10:15:20 +0500

    Add file preview plugin

---
+++ modules/theme_inspector_form/src/Plugin/ThemePreview/File.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\theme_inspector_form\Plugin\ThemePreview;
+
+/**
+ * File input preview.
+ *
+ * @ThemePreview(
+ *   id = "file",
+ *   label = @Translation("File"),
+ *   category = @Translation("Form Elements"),
+ *   variations = {
+ *     "default" = @Translation("Default"),
+ *     "disabled" = @Translation("Disabled"),
+ *   },
+ * )
+ */
+final class File extends FormElementBase {
+
+  protected function getElement(string $variation): array {
+    return [
+      '#title' => \ucfirst($this->random()->name()),
+      '#type' => 'file',
+      '#description' => $this->random()->sentences(1),
+      '#disabled' => $variation === 'disabled',
+    ];
+  }
+
+}
